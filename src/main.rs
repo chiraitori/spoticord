e847ad18a2a1568a9a916179f9aed5c409034059
Chiraitori <77224364+chiraitori@users.noreply.github.com>
2024-10-21 17:38:28 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,32 @@ use poise::Framework;
 use serenity::all::ClientBuilder;
 use songbird::SerenityInit;
 use spoticord_database::Database;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::{TcpStream, TcpListener};
+use std::io::{Read, Write};
+use std::thread;
 
-async fn hello() -> HttpResponse {
-    HttpResponse::Ok().body("Hello World from Spoticord!")
+fn handle_read(mut stream: &TcpStream) {
+    let mut buf = [0u8; 4096];
+    match stream.read(&mut buf) {
+        Ok(_) => {
+            let req_str = String::from_utf8_lossy(&buf);
+            println!("{}", req_str);
+        },
+        Err(e) => println!("Unable to read stream: {}", e),
+    }
+}
+
+fn handle_write(mut stream: TcpStream) {
+    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world from Spoticord!</body></html>\r\n";
+    match stream.write(response) {
+        Ok(_) => println!("Response sent"),
+        Err(e) => println!("Failed sending response: {}", e),
+    }
+}
+
+fn handle_client(stream: TcpStream) {
+    handle_read(&stream);
+    handle_write(stream);
 }
 
 #[tokio::main]
@@ -17,6 +39,7 @@ async fn main() {
     // Since multiple dependencies either enable aws_lc_rs or ring, they cause a clash, so we have to
     // explicitly tell rustls to use the aws-lc-rs provider
     * = rustls::crypto::aws_lc_rs::default_provider().install_default();
+    
     // Setup logging
     if std::env::var("RUST_LOG").is_err() {
         #[cfg(debug_assertions)]
@@ -28,6 +51,7 @@ async fn main() {
     info!("Today is a good day!");
     info!(" - Spoticord");
     dotenvy::dotenv().ok();
+    
     // Set up database
     let database = match Database::connect().await {
         Ok(db) => db,
@@ -36,11 +60,13 @@ async fn main() {
             return;
         }
     };
+    
     // Set up bot
     let framework = Framework::builder()
         .setup(|ctx, ready, framework| Box::pin(bot::setup(ctx, ready, framework, database)))
         .options(bot::framework_opts())
         .build();
+    
     let mut client = match ClientBuilder::new(
         spoticord_config::discord_token(),
         spoticord_config::discord_intents(),
@@ -56,19 +82,25 @@ async fn main() {
         }
     };
 
-    // Start HTTP server
-    let http_server = HttpServer::new(|| {
-        App::new().route("/", web::get().to(hello))
-    })
-    .bind("127.0.0.1:10000")
-    .expect("Can not bind to port 10000");
-
-    // Run bot and HTTP server concurrently
-    tokio::spawn(async move {
-        info!("HTTP server running at http://localhost:10000");
-        http_server.run().await.expect("HTTP server failed");
+    // Start the HTTP server in a separate thread
+    thread::spawn(|| {
+        let listener = TcpListener::bind("0.0.0.0:10000").unwrap();
+        info!("HTTP server listening for connections on all interfaces, port 10000");
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    thread::spawn(|| {
+                        handle_client(stream)
+                    });
+                }
+                Err(e) => {
+                    error!("Unable to connect: {}", e);
+                }
+            }
+        }
     });
 
+    // Start the Discord bot
     if let Err(why) = client.start_autosharded().await {
         error!("Fatal error occurred during bot operations: {why}");
         error!("Bot will now shut down!");
